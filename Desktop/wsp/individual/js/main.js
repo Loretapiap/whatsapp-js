a9c978ecda22ca032a5c006ce2e5dd793da82187
Loretapiap <loretapiap@gmail.com>
2016-07-18 13:17:55 -0400

    parte de los emojis falta, hecho el creador de texto en chat, faltan arreglos en el subir imagen

---
+++ Desktop/wsp/individual/js/main.js
@@ -0,0 +1,46 @@
+/*Escritura en el chat*/
+$(document).ready(function(){
+    $('#mainInput').keypress(function(e){
+        if(e.which == 13){
+            var value = $('#mainInput').val()
+            var time = new Date(),
+            hours = time.getHours(),
+            minutes = time.getMinutes()
+            $('.w-message-list').append('<div class="w-message w-message-out "><div class="w-message-text"><p>' + value + '</p><div class="time">' + hours + ':' + minutes + '</div></div></div>')
+            $('#mainInput').val('')
+        }
+    })
+})
+
+$('.avatar').click(function(){
+    $('.w-message-list').empty()
+})
+
+/*cargar imagenes*/
+
+$(window).load(function(){
+
+ $(function() {
+  $('#file-input').change(function(e) {
+      addImage(e); 
+     });
+
+     function addImage(e){
+      var file = e.target.files[0],
+      imageType = /image.*/;
+    
+      if (!file.type.match(imageType))
+       return;
+  
+      var reader = new FileReader();
+      reader.onload = fileOnload;
+      reader.readAsDataURL(file);
+     }
+
+  
+     function fileOnload(e) {
+      var result=e.target.result;
+      $('#imgSalida').attr("src",result);
+     }
+    });
+  });
