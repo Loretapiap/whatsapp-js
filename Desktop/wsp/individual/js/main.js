38eee2830ae6ec468037e2facbe803543bf1d2f1
Loretapiap <loretapiap@gmail.com>
2016-07-19 13:42:10 -0400

    arreglados problemas de sintaxis

--- Desktop/wsp/individual/js/main.js
+++ Desktop/wsp/individual/js/main.js
@@ -1,11 +1,8 @@
 /*Escritura en el chat*/
-
-/****Ahora que borre todos los emojis no me funciona el enviar ): lo siento ****/
-/*
 $(document).ready(function(){
-    $('.w-write-message-field').keypress(function(e){
+    $('#mainInput').keypress(function(e){
         if(e.which == 13){
-            var value = $('.w-write-message-field').val()
+            var value = $('#mainInput').val()
             var time = new Date(),
             hours = time.getHours(),
             minutes = time.getMinutes()
@@ -18,40 +15,9 @@ $(document).ready(function(){
 $('.avatar').click(function(){
     $('.w-message-list').empty()
 })
+
+
 /*nuevos*/
-
-
-$(document).ready(function(){
-    llenado()
-});
-
-
-$("#search").keyup(function(){
-  var texto=$("#search").val()
-  $.getJSON("data.json",function(data){
-    var output="";
-    $.each(data,function(key,val){
-      if(val.nombre.search(texto) != -1 && texto!=""){
-      output+="<div class='panel-list-message-contact'><div class='profile-img'><img src='image/"+val.imagen+"'/></div><div class='message-text show-dots'><div class='message-text-detail'><div class='name-contact show-dots'>"+val.nombre+"</div><div class='message-hour '>"+val.mensajes[0].fecha+"</div></div><div class='message-chat show-dots'><p class='show-dots'>"+val.mensajes[0].contenido+"</p></div></div></div>"
-      
-      }
-    })
-    $(".w-search-contacts").html(output)
-  });
-
-});
-
-function llenado(){
-  $.getJSON("data.json",function(data){
-    var output="";
-    $.each(data,function(key,val){
-      output+="<div class='panel-list-message-contact'><div class='profile-img'><img src='image/"+val.imagen+"'/></div><div class='message-text show-dots'><div class='message-text-detail'><div class='name-contact show-dots'>"+val.nombre+"</div><div class='message-hour '>"+val.mensajes[0].fecha+"</div></div><div class='message-chat show-dots'><p class='show-dots'>"+val.mensajes[0].contenido+"</p></div></div></div>"
-
-    })
-    $(".w-search-contacts").html(output)
-    
-  });
-}
 
 
 /*cargar imagenes*/
